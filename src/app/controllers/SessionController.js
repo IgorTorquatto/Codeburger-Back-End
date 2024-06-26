2b6bb3672f7dcb55831480ad14632568c6de0010
Igor <igor.torquato.20@gmail.com>
2024-06-26 19:32:24 -0300

    feat: feat: add token support in the backend

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,35 +1,46 @@
 import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
 import User from '../models/User'
+import authConfig from '../../config/auth'
 
-class SessionController{
-    async store(request,response){
+class SessionController {
+    async store(request, response) {
         const schema = Yup.object().shape({
             email: Yup.string().email().required(),
             password: Yup.string().required(),
         })
 
-    const userEmailorPasswordIncorrect = ()=>{
-        return response.status(400).json({error: "Tenha certeza de que sua senha ou email estejam corretos"})
-    }
-    if( !(await schema.isValid(request.body))){
-        userEmailorPasswordIncorrect()
-    }
+        const userEmailorPasswordIncorrect = () => {
+            return response.status(400).json({ error: "Tenha certeza de que sua senha ou email estejam corretos" })
+        }
+        if (!(await schema.isValid(request.body))) {
+            userEmailorPasswordIncorrect()
+        }
 
-    const { email, password } = request.body
+        const { email, password } = request.body
 
-    const user = await User.findOne({
-        where: { email },
-    })
+        const user = await User.findOne({
+            where: { email },
+        })
 
-    if(!user){
-        userEmailorPasswordIncorrect()
-    }
+        if (!user) {
+            userEmailorPasswordIncorrect()
+        }
 
-    if(!(await user.checkPassword(password))){
-        userEmailorPasswordIncorrect()
-    }
+        if (!(await user.checkPassword(password))) {
+            userEmailorPasswordIncorrect()
+        }
 
-    return response.json({id: user.id, email, name: user.name, admin: user.admin,})
+        return response.json({ id: user.id,
+            email,
+            name: user.name,
+            admin: user.admin,
+            token: jwt.sign(
+                {id: user.id},
+                authConfig.secret,
+                {expiresIn: authConfig.expiresIn}
+            )
+        })
     }
 }
 
